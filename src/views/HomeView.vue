51fd55073f092b9d5001fbec608e432e86f32ca0
Tobias Knöppler <6317548+theCalcaholic@users.noreply.github.com>
2026-04-03 04:05:51 +0200

    HomeView.vue: Allow providing backend config via environment variable

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,10 +7,21 @@ import {ref, onMounted, computed} from "vue";
 const config: Ref<{ "apiUrl": string, "apiRedirect": string, "categoryFilter": string }|undefined> = ref(undefined);
 
 onMounted(async () => {
-  const response = await fetch("/config.json");
-  if (! response.ok)
-    throw Error("ERROR: Failed to fetch configuration!")
-  config.value = {...config.value, ...(await response.json())};
+  try {
+    const response = await fetch("/config.json");
+    if (! response.ok) {
+      throw Error("ERROR: Failed to fetch configuration!")
+    }
+    config.value = {...config.value, ...(await response.json())};
+  } catch (e) {
+    if (import.meta.env.VITE_DEVELOPMENT_CONFIG) {
+      // development setup
+      console.log(import.meta.env.VITE_DEVELOPMENT_CONFIG)
+      config.value = JSON.parse(import.meta.env.VITE_DEVELOPMENT_CONFIG);
+    } else {
+      throw e;
+    }
+  }
 })
 
 </script>
